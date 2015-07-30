1ce25e673c051c0c6b3e7457cdf7a77934f8a715
Alejandro Álvarez Ayllón <a.alvarezayllon@gmail.com>
2015-07-30 16:22:31 +0200

    gfal2-python and gfal2-util

---
+++ gfal2-util.rb
@@ -0,0 +1,17 @@
+class Gfal2Util< Formula
+  desc "Grid file access library 2, command line tools"
+  homepage "http://dmc.web.cern.ch/"
+  url "https://gitlab.cern.ch/dmc/gfal2-util.git", :branch => "develop"
+  version "1.3.0"
+
+  depends_on "gfal2-python"
+
+  def install
+    system "python", "setup.py", "install"
+  end
+
+  test do
+    system "#{bin}/gfal-ls file:///"
+  end
+end
+
