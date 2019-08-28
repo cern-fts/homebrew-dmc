e43c44ea804da92d373991b407a7d55a970c8489
Andrea Manzi <andrea.manzi@gmail.com>
2019-08-28 22:53:10 +0200

    Update gfal2.rb

--- gfal2.rb
+++ gfal2.rb
@@ -7,7 +7,7 @@ class Gfal2 < Formula
 
   depends_on "boost"
   depends_on "cmake" => :build
-  depends_on "davix" => :build
+  depends_on "cern-fts/dmc/davix" => :build
   depends_on "json-c"
   depends_on "glib"
   depends_on "globus-toolkit"
