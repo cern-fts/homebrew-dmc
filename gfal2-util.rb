63999a36da62e7584ff6199ee575cf721e63c3ed
Andrea Manzi <andrea.manzi@gmail.com>
2019-08-28 22:57:23 +0200

    Update gfal2-util.rb

--- gfal2-util.rb
+++ gfal2-util.rb
@@ -1,8 +1,9 @@
 class Gfal2Util< Formula
   desc "Grid file access library 2, command line tools"
   homepage "http://dmc.web.cern.ch/"
-  url "https://gitlab.cern.ch/dmc/gfal2-util.git", :branch => "develop"
-  version "1.5.1"
+  url "https://gitlab.cern.ch/dmc/gfal2-util.git", 
+      :tag => "v1.5.3"
+  version "1.5.3"
 
   depends_on "gfal2-python"
 
